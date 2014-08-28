a37e6cd4479097c8a4740d8ec7641edbbb7027a9
cloverhsc <clover.hsc@gmail.com>
2014-08-28 18:57:00 +0800

    add count reboot times by watch dog
    add count reboot times by watch dog and main flow.
    need add send mail by ruby in nas.

--- watchdog.sh
+++ watchdog.sh
@@ -25,7 +25,7 @@
 function find_broken_process()
 {
 
-#record process state in now
+#record process state now
 if [ -f /nas/tmp/proc_record$1.txt ];then
     rm /nas/tmp/proc_record$1.txt 2>/dev/null
 fi
@@ -33,7 +33,7 @@ touch /nas/tmp/proc_record$1.txt
 
 test ! -f /nas/tmp/proc_ref$1.txt && touch /nas/tmp/proc_ref$1.txt		# record state at last time
 
-declare -i count=0         #count process.
+declare -i count=0         #count every processes Dead or Zombie times.
 need_reboot="false"
 #process state on Zombie or Dead
 psx_state=$1
@@ -62,7 +62,11 @@ done< /nas/tmp/psx.result
 		count=`echo $line|awk '{print $4}'`
 		if [ $count -gt 10 ];then
 			echo `date "+%Y %m %d %H:%M:%S"` >> /DOM/.wdog.log
-			echo "Find Zombie process $line !" >> /DOM/.wdog.log
+			if [ "$1" == "Z" ];then
+				echo "Find Zombie process $line !" >> /DOM/.wdog.log
+			elif [ "$1" == "D" ];then
+				echo "Find Dead process $line !" >> /DOM/.wdog.log
+			fi
 			echo "" >>/DOM/.wdog.log
 			need_reboot="true"
 		fi
@@ -88,7 +92,7 @@ function find_filesystem_error()
 {
 	if str=`grep "XFS internal error" /nas/tmp/dmesg.result`;then
 		`echo >>/DOM/.wdog.log`
-		echo -e`date`"\n" Detect XFS ERROR : $str >> /DOM/.wdog.log
+		echo -e`date "+%Y %m %d %H:%M:%S"`"\n" Detect XFS ERROR : $str >> /DOM/.wdog.log
 		touch /DOM/.fsrepair
 		echo "Detect XFS error! Reboot for file system check !"
 		do_reboot
@@ -96,7 +100,7 @@ function find_filesystem_error()
 	
 	if str=`grep "EXT3-fs error" /nas/tmp/dmesg.result`;then
 		`echo >>/DOM/.wdog.log`
-		echo -e`date`"\n" Detect EXT3-fs ERROR : $str >> /DOM/.wdog.log
+		echo -e`date "+%Y %m %d %H:%M:%S"`"\n" Detect EXT3-fs ERROR : $str >> /DOM/.wdog.log
 		touch /DOM/.fsrepair
 		echo "Detect EXT3-fs error ! Reboot for file system check!"
 		do_reboot
@@ -104,7 +108,7 @@ function find_filesystem_error()
 	
 	if str=`grep "EXT4-fs error" /nas/tmp/dmesg.result`;then
 		`echo >>/DOM/.wdog.log`
-		echo -e`date`"\n" Detect EXT4 ERROR : $str >> /DOM/.wdog.log
+		echo -e`date "+%Y %m %d %H:%M:%S"`"\n" Detect EXT4 ERROR : $str >> /DOM/.wdog.log
 		touch /DOM/.fsrepair
 		echo "Detect EXT4 error! Reboot for file system check!"
 		do_reboot
@@ -112,7 +116,7 @@ function find_filesystem_error()
 	
 	if str=`grep "Kernel panic" /nas/tmp/dmesg.result`;then
 		`echo >>/DOM/.wdog.log`
-		echo -e`date`"\n" Detect KERNEL PANIC ERROR : $str >> /DOM/.wdog.log
+		echo -e`date "+%Y %m %d %H:%M:%S"`"\n" Detect KERNEL PANIC ERROR : $str >> /DOM/.wdog.log
 		touch /DOM/.fsrepair
 		echo "Detect Kernel panic ! Reboot !"
 		do_reboot
@@ -120,7 +124,7 @@ function find_filesystem_error()
 	
 	if str=`grep "Call Trace" /nas/tmp/dmesg.result`;then
 		`echo >>/DOM/.wdog.log`
-		echo -e`date`"\n" Detect KERNEL CALL TRACE ERROR : $str >> /DOM/.wdog.log
+		echo -e`date "+%Y %m %d %H:%M:%S"`"\n" Detect KERNEL CALL TRACE ERROR : $str >> /DOM/.wdog.log
 		touch /DOM/.fsrepair
 		echo "Detect kernel call trace error! Reboot"
 		do_reboot
@@ -130,10 +134,49 @@ function find_filesystem_error()
 
 function do_reboot()
 {
+	test ! -f /DOM/.wdogrbt.log && echo "WatchDog reboot count:0" > /DOM/.wdogrbt.log
 	touch /nas/tmp/hwfreboot     # stopAllSvc will not stop bash
+	declare -i count=`cat /DOM/.wdogrbt.log |awk -F ":" '{print $2}'`
+	count=$(( $count + 1 ))
+	echo "WatchDog reboot count:$count" > /vol/test/count
 	`sync &`
 	`stopAllSvc`
 	`sleep 10`
 	`forcereboot`
 }
+
+# ------------------Start here--------------------------
+test ! -f /DOM/.wdogrbt.log && echo "WatchDog reboot count:0" > /DOM/.wdogrbt.log
+test ! -f /DOM/.nasboot && touch /DOM/.nasboot
+test -f /DOM/.fsrepair && rm /DOM/.fsrepair 2>/dev/null
+declare -i count=`cat /vol/test/count |awk -F ":" '{print $2}'`
+
+#every 2 mins clear watch dog. So every 10 clear times => pass by 20 mins
+declare -i is20mins=0;				
+
+for ((;;))
+do
+		#first check reboot times by watch dog
+		if [ "$count" -gt "3" ];then
+			echo `date "+%Y %m %d %H:%M:%S"` >> /DOM/.wdog.log
+			echo "reboot over 3 times. Stop watch dog !" >> /DOM/.wdog.log
+			`hwdog off`
+			break
+		fi
+		
+		#if system run over 20 mins. Clean /DOM/.wdogrbt.log count=0 
+		if [ "$is20mins" -gt "9" ];then
+			is20mins=0
+			echo "WatchDog reboot count:0" > /DOM/.wdogrbt.log
+		fi
+		
+        `ps -o pid,tty,stat,user,time,args > /nas/tmp/psx.result`   # equal to psx > /nas/tmp/psx.result
+        find_broken_process Z
+        find_broken_process D
+        cc=`expr $cc + 1 `
+        echo $cc
+        sleep 120
+		is20mins=$(( $is20mins + 1 ))
+done
+
 
